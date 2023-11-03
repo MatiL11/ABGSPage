08980316ccd2f596d4899f0a5aff46402bca370b
Matias <matiaslisandro9@gmail.com>
2023-11-03 16:18:04 -0300

    modificacionInfobar

--- src/components/Infobar/index.js
+++ src/components/Infobar/index.js
@@ -3,10 +3,65 @@ import { FlexContainer } from "../../styles/globalStyles";
 import { InfoBarContainer } from "./styles";
 
 export const Infobar = () => {
+  const bodyTitle = "OUR SERVICES";
+
   return (
     <InfoBarContainer>
       <FlexContainer>
-        <h1>OUR SERVICES</h1>
+        <h1>{bodyTitle}</h1>
+      </FlexContainer>
+      <FlexContainer row={true}>
+        <FlexContainer>
+          <p>
+            We offer a wide range of services to meet all your transportation
+            needs. Our services include:
+          </p>
+
+          <ul>
+            <li>International Air Freight</li>
+            <li>International Ocean Freight</li>
+            <li>Ground Freight Services</li>
+            <li>Customs Clearance</li>
+            <li>Warehousing</li>
+            <li>Insurance</li>
+            <li>Door to Door</li>
+            <li>Port to Port</li>
+          </ul>
+        </FlexContainer>
+        <FlexContainer>
+          <p>
+            We offer a wide range of services to meet all your transportation
+            needs. Our services include:
+          </p>
+
+          <ul>
+            <li>International Air Freight</li>
+            <li>International Ocean Freight</li>
+            <li>Ground Freight Services</li>
+            <li>Customs Clearance</li>
+            <li>Warehousing</li>
+            <li>Insurance</li>
+            <li>Door to Door</li>
+            <li>Port to Port</li>
+          </ul>
+        </FlexContainer>
+        <FlexContainer>
+          <p>
+            We offer a wide range of services to meet all your transportation
+            needs. Our services include:
+          </p>
+
+          <ul>
+            <li>International Air Freight</li>
+            <li>International Ocean Freight</li>
+            <li>Ground Freight Services</li>
+            <li>Customs Clearance</li>
+            <li>Warehousing</li>
+            <li>Insurance</li>
+            <li>Door to Door</li>
+            <li>Port to Port</li>
+          </ul>
+        </FlexContainer>
       </FlexContainer>
     </InfoBarContainer>
   );
